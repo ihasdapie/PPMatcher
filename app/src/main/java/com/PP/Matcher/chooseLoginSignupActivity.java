5d4fa1bf862e587c2ef599231f0bc90494f262bc
Brian <ihasdapi@gmail.com>
2020-02-25 21:59:53 -0800

    Enabled account creation and login tools with Firebase Auth
    Signed-off-by: Brian <ihasdapi@gmail.com>

---
+++ app/src/main/java/com/PP/Matcher/chooseLoginSignupActivity.java
@@ -0,0 +1,49 @@
+package com.PP.Matcher;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class chooseLoginSignupActivity extends AppCompatActivity {
+
+	private Button mLogin, mSignup;
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_choose_login_signup);
+
+		mLogin = findViewById(R.id.activityChooseLoginSignup_loginButton);
+		mSignup = findViewById(R.id.activityChooseLoginSignup_signupButton);
+
+		mLogin.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(chooseLoginSignupActivity.this, loginActivity.class);
+				startActivity(intent);
+				finish();
+				return;
+			}
+		});
+
+		mSignup.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(chooseLoginSignupActivity.this, signupActivity.class);
+				startActivity(intent);
+				finish();
+				return;
+
+			}
+		});
+
+
+
+
+	}
+}
+

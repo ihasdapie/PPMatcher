c1ddb47a2643ed7eac165bbed6e24e618deb27d6
Brian <ihasdapi@gmail.com>
2020-02-29 00:28:14 -0800

    added ppCreator (personal profile) Activity started database integration updated loginActivity, signupActivity with better input fields
    Signed-off-by: Brian <ihasdapi@gmail.com>

---
+++ app/src/main/java/com/PP/Matcher/ppCreatorActivity.java
@@ -0,0 +1,14 @@
+package com.PP.Matcher;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ppCreatorActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_pp_creator);
+	}
+}

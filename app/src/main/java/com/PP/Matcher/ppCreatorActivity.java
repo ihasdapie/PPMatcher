a5b7d89391fcd290afe175bbd471d9956f0789d3
Brian <ihasdapi@gmail.com>
2020-03-01 14:53:04 -0800

    minor xml edits

--- app/src/main/java/com/PP/Matcher/ppCreatorActivity.java
+++ app/src/main/java/com/PP/Matcher/ppCreatorActivity.java
@@ -1,13 +1,24 @@
 package com.PP.Matcher;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ppCreatorActivity extends AppCompatActivity {
 
-	private Spinner
+	private Spinner mUserFaculty;
+	private EditText mUserFirstName, mUserLastName, mUserYear, mUserAverage,
+			mPrompt1, mPrompt2, mPrompt3, mResponse1, mResponse2, mResponse3;
+	private Button mCreateProfile;
+	private CheckBox mTermsOfService;
+	private SeekBar mUserInterestSimilarity;
+
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
